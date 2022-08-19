1b5673b78da57384c044b4328cb7580441cb8924
lyangenda <lyangendadavid@gmail.com>
2022-08-19 18:44:51 +0200

    add additional tests and make final changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  root 'home#index'
+  resources :categories, only: %i[index show create new destroy update] do
+    resources :transancts, only: %i[index show create new destroy update]
+  end
 end
